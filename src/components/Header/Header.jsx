d102830ed1d55fac536afd52b39e8e48b0b6775e
Viktoriia <133902609+vivinohradova@users.noreply.github.com>
2024-01-29 19:58:43 +0200

    adaptive for header, main,services, partly about me

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,32 @@
 import logo from "../../assets/images/logo.png";
 import styles from "./Header.module.scss";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
   return (
     <>
-    <header>
-      <div className={styles.container}>
-        <img className={styles.logo} src={logo} alt="logo" />
-        <nav className={styles.menu}>
-          <li>
-            <button type="button">Послуги</button>
-          </li>
-          <li>
-            <button type="button">Про мене</button>
-          </li>
-          <li>
-            <button type="button">Відгуки</button>
-          </li>
-          <li>
-            <button type="button">Контакти</button>
-          </li>
-        </nav>
-      </div>
+      <header>
+        <div className={styles.container}>
+          <img className={styles.logo} src={logo} alt="logo" />
+          <nav className={styles.menu}>
+            <li>
+              <button type="button">Послуги</button>
+            </li>
+            <li>
+              <button type="button">Про мене</button>
+            </li>
+            <li>
+              <button type="button">Відгуки</button>
+            </li>
+            <li>
+              <button type="button">Контакти</button>
+            </li>
+          </nav>
+
+          <nav className={styles.menu_mobile}>
+            <GiHamburgerMenu className={styles.gamburger} />
+          </nav>
+        </div>
       </header>
     </>
   );
