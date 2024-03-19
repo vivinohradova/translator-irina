8baaac74efc027091da074629d7b59704c7e3a84
Viktoriia <133902609+vivinohradova@users.noreply.github.com>
2024-03-19 13:56:28 +0200

    humburger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,16 @@ import logo from "../../assets/images/logo.png";
 import { useLanguage } from "../LanguageContext";
 import scrollToElement from "../ScrollUtils/ScrollUtils";
 import styles from "./Header.module.scss";
-// import { GiHamburgerMenu } from "react-icons/gi";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { useTranslation } from "react-i18next";
 import { useEffect, useState } from "react";
 
 const Header = () => {
   const { changeLanguage } = useLanguage();
   const { t } = useTranslation();
+
   const [selectedLanguage, setSelectedLanguage] = useState("ua");
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     changeLanguage(selectedLanguage);
@@ -20,7 +22,13 @@ const Header = () => {
   const handleClick = (e, targetId) => {
     e.preventDefault();
     scrollToElement(targetId);
+    setIsOpen(false);
   };
+
+  const handleClickHamburger = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <header>
@@ -75,9 +83,42 @@ const Header = () => {
               </a>
             </li>
           </nav>
-          {/* <nav className={styles.menu_mobile}>
-            <GiHamburgerMenu className={styles.gamburger} />
-          </nav> */}
+          <div className={styles.mobile}>
+            <GiHamburgerMenu
+              className={styles.gamburger}
+              onClick={handleClickHamburger}
+            />
+            <nav className={` ${isOpen ? styles.menu_mobile : styles.menu}`}>
+              <li>
+                <a href="#services" onClick={(e) => handleClick(e, "services")}>
+                  <button type="button">{t("menuService")}</button>
+                </a>
+              </li>
+              <li>
+                <a href="#about" onClick={(e) => handleClick(e, "about")}>
+                  <button type="button">{t("menuAboutMe")}</button>
+                </a>
+              </li>
+
+              <li>
+                <a href="#projects" onClick={(e) => handleClick(e, "projects")}>
+                  <button type="button">{t("menuProjects")}</button>
+                </a>
+              </li>
+
+              <li>
+                <a href="#reviews" onClick={(e) => handleClick(e, "reviews")}>
+                  <button type="button">{t("menuReviews")}</button>
+                </a>
+              </li>
+
+              <li>
+                <a href="#contacts" onClick={(e) => handleClick(e, "contacts")}>
+                  <button type="button">{t("menuContact")}</button>
+                </a>
+              </li>
+            </nav>
+          </div>
         </div>
       </header>
     </>
